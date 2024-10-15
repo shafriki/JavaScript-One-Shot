2a35bc33e067d001329da84ec0138128f1e10be4
shafriki <shafriki.cse@gmail.com>
2024-10-15 15:45:53 +0600

    complete variable and data types part

---
+++ index.js
@@ -0,0 +1,89 @@
+// variable and data types
+
+// alert('shafriki islam');
+
+/*console.log('shafriki islam a new learner');
+
+name = 'shafriki islam';
+age ='25';
+degree = 'B.Sc in CSE';
+console.log(name, age, degree);
+*/
+
+// var let const
+
+/*
+let a =10;
+let b =20;
+const result = a+b;
+let Result = a-b;
+console.log(result,Result);*/
+
+//data type in JavaScript
+
+/*let number = 05;
+const fullName = 'shafriki islam';
+const age ='25';
+const phoneNumber = 01786141015;
+console.log(number, fullName, age, phoneNumber);*/
+
+// object task 1
+/*const product = {
+    productTitle:'Parker Jotter Standard CT Ball Pen(Black)',
+    price: 270,
+    rating: 7,
+    isDeal: true,
+    offer: '5%'
+
+}
+console.log(product);
+console.log(product.price);
+console.log(product['productTitle']);
+
+// object task 2
+const profile = {
+    userName: 'ShradhaKhapra',
+    isFollow: true,
+    followers: '569K',
+    following: 4,
+    post: '195 posts',
+    bio: 'apna college'
+}
+console.log(profile);
+console.log(profile.post);
+console.log(typeof profile);*/
+
+// 1. Declare and Initialize Variables
+/*const firstName = 'Shafriki';
+const lastName = 'Islam';
+const age = 25;
+console.log(`my name is ${firstName} ${lastName}. my age: ${age}`);
+
+// task 2
+const fullName = firstName +' '+ lastName;
+console.log(fullName);*/
+
+// task 3
+// let isEmployed = true;
+
+// console.log(isEmployed);
+
+// task 4
+/*let hobbies = ['bike riding', 'sleeping', 'coding'];
+hobbies.push('eating');
+console.log(hobbies)*/
+
+// task 5
+const person = {
+    firstName: 'shafriki',
+    lastName: 'Islam',
+    age: 25,
+    hobbies: 'bike riding'
+};
+person.age =26;
+console.log(person);
+console.log(person.firstName);
+console.log(typeof person.age);
+console.log(person['hobbies'])
+
+
