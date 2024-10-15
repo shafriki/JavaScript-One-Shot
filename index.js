3b368ffa2eb5105fffd205185a1cd3474bbba4f9
shafriki <shafriki.cse@gmail.com>
2024-10-15 18:09:07 +0600

    operators and conditional statement

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// variable and data types
+// chapter -1: variable and data types
 
 // alert('shafriki islam');
 
@@ -74,16 +74,126 @@ hobbies.push('eating');
 console.log(hobbies)*/
 
 // task 5
-const person = {
+/*const person = {
     firstName: 'shafriki',
     lastName: 'Islam',
     age: 25,
     hobbies: 'bike riding'
 };
 person.age =26;
+
 console.log(person);
 console.log(person.firstName);
 console.log(typeof person.age);
 console.log(person['hobbies'])
+*/
+
+
+// chapter -2: Operators and Conditional Statements
+
+// arithmetic operator 
+
+/*let a =6;
+let b =2;
+let sum = a+b;
+let sub = a-b;
+let multiply = a*b;
+let divide = a/b;
+
+console.log(`the value of a = ${a} and b = ${b}. Here a and b sum = ${sum}, subtract = ${sub}, multiply = ${multiply}, and divide = ${divide},`);*/
+
+// unary operator
+/*let a =100;
+let b =200;
+
+console.log(`a-- = ${a--}`);
+console.log(`a = ${a}`);*/
+
+// assignment operator
+let a = 100;
+a%=10;
+console.log(a);
+
+// conditional statement
+/*let num = 109493283;
+
+if(num%2 == 0) {
+    console.log('EVEN Number')
+}
+else {
+    console.log('ODD Number')
+}
+
+let number = prompt('Enter a number');
+
+if(number % 5 == 0) {
+    console.log('multiple of 5')
+}
+else{
+    console.log('not')
+}*/
+
+/*let marks = 9;
+
+if(marks >= 80 && marks <= 100) {
+    console.log('Grade: A')
+}
+else if(marks >= 70 && marks <= 89) {
+    console.log('Grade: B')
+}
+else if(marks >= 60 && marks <= 69) {
+    console.log('Grade: D')
+}
+else if(marks >= 50 && marks <= 59) {
+    console.log('Grade: F')
+}
+else {
+    console.log('FAILED')
+}*/
+
+// task challenge 1-100
+let firstName = 'Shafriki';
+let lastName = 'Islam';
+console.log(`My name is ${firstName} ${lastName}`);
+
+// task challenge 2-100
+const birthYear = 1999;
+const currentYear = 2024;
+const age = currentYear-birthYear;
+console.log(`I am ${age} years old`);
+
+// task challenge 3-100
+const favoriteColor = 'black';
+console.log(`my favorite color is ${favoriteColor}`);
+
+// task challenge 4-100
+const favFood = ['honey','kacchi biriani', 'milk', 'beaf meet cury', 'bou'];
+console.log(favFood[1]);
+
+// task challenge 5-100
+const person = {
+    firstName:'shafriki',
+    lastName: 'islam',
+    age: 25
+};
+console.log(`my name is ${firstName} ${lastName}. I am ${age} years old`);
+
+// task challenge 6-100
+let h = 5;
+let i = 10;
+
+let temp = h;
+h = i;
+i = temp;
+
+console.log(h, i);
+
+// task challenge 8 -100
+if(isWeekend == 'saturday' || 'sunday') {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
 
 
